cf426f6c1ef359c4f34153a56b75c4c014898264
Boris Muzalevskii <bmuza57@gmail.com>
2022-10-26 16:15:33 +0300

    Add html for legendaries page

---
+++ src/pages/Legendaries/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const Legendaries = () => {
+  return (
+    <main>
+      <h1>Legendaries</h1>
+      <div>
+        <div>
+          <img />
+        </div>
+        <div>
+          <h2></h2>
+          <p></p>
+          <div>
+            <ul>
+              <li></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div>
+        <ul>
+          <li></li>
+        </ul>
+      </div>
+    </main>
+  );
+};
+
+export default Legendaries;
