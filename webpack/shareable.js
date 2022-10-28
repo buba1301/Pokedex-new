18b47f4bf6c39c9e163e6a93e8816c076a98eab1
Boris Muzalevskii <bmuza57@gmail.com>
2022-10-28 22:36:47 +0300

    Fix styles

--- webpack/shareable.js
+++ webpack/shareable.js
@@ -8,7 +8,7 @@ module.exports = {
       '@pages': path.resolve('src', 'pages'),
       '@components': path.resolve('src', 'components'),
       '@layout': path.resolve('src', 'Layout'),
-      '@variables': path.resolve(__dirname, '_variables'),
+      '@variables': path.resolve('_variables.scss'),
       utils: path.resolve('src', 'utils'),
       store: path.resolve('src', 'store.js'),
     },
