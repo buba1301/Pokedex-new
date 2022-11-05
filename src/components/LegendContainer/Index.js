2c9326c9db6cfa2a5d6cedb4a5182668a28e5cd6
Boris Muzalevskii <bmuza57@gmail.com>
2022-11-05 18:39:50 +0300

    Create new components for Legend page

--- src/components/LegendContainer/Index.js
+++ src/components/LegendContainer/Index.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import Description from './Description/Description';
 
 import s from './LegendContainer.module';
+import Slider from './Slider/Slider';
 
 const Legend = () => {
   console.log('Rendering Legend');
@@ -9,13 +11,13 @@ const Legend = () => {
     <section className={s.root}>
       <h1>Legendaries</h1>
       <div className={s.info}>
-        <div className={s.imgContainer}>pss</div>
-        <div className={s.descriptions}>pss</div>
+        <div className={s.imgContainer}>Img</div>
+        <div className={s.descriptions}>
+          <Description />
+        </div>
       </div>
       <div className={s.sliderWrap}>
-        <ul>
-          <li />
-        </ul>
+        <Slider />
       </div>
     </section>
   );
---
+++ src/components/LegendContainer/Slider/Slider.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import s from './Slider.module';
+
+const Slider = () => {
+  console.log('RENDER Slider');
+
+  return <div>Slider</div>;
+};
+
+export default Slider;
