0741174bc44e14b38b09e2a8983a126f578c8369
Boris Muzalevskii <bmuza57@gmail.com>
2022-11-05 18:33:53 +0300

    Add block for slider

--- src/components/LegendContainer/Index.js
+++ src/components/LegendContainer/Index.js
@@ -3,14 +3,16 @@ import React from 'react';
 import s from './LegendContainer.module';
 
 const Legend = () => {
+  console.log('Rendering Legend');
+
   return (
     <section className={s.root}>
       <h1>Legendaries</h1>
       <div className={s.info}>
-        <div className={s.imgContainer}></div>
-        <div className={s.descriptions}></div>
+        <div className={s.imgContainer}>pss</div>
+        <div className={s.descriptions}>pss</div>
       </div>
-      <div>
+      <div className={s.sliderWrap}>
         <ul>
           <li />
         </ul>
