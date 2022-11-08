50334e8d6962ee02a125d8d7ea88f175fcb26278
Boris Muzalevskii <bmuza57@gmail.com>
2022-11-08 17:32:26 +0300

    fix

--- src/pages/Pokedex/index.js
+++ src/pages/Pokedex/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import PokemonsContainer from '../../components/PokemonsContainer';
 
-<<<<<<< HEAD:src/pages/Pokedex/index.js
-=======
 export const loader = async () => {
   const pokemonsList = await config.getPokemons();
   return { pokemonsList };
@@ -10,7 +8,6 @@ export const loader = async () => {
 
 //TODO: как запрашивать необходиое количество в зависимости от размера экрана
 
->>>>>>> b02ed70... Add filters buttons and checkbox:src/roots/Pokedex/index.js
 const Pokedex = () => {
   console.log('RENDER POKEDEX');
   return (
