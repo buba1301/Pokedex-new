159ea624a38d266aa6aff3a29de35819c12de419
Boris Muzalevskii <bmuza57@gmail.com>
2022-11-08 17:26:02 +0300

    Add filters buttons and checkbox

--- src/components/PokemonsContainer/Filters/index.js
+++ src/components/PokemonsContainer/Filters/index.js
@@ -1,13 +1,38 @@
-import React from 'react';
+import classNames from 'classnames';
+import React, { useState } from 'react';
+
+import s from './Filter.module';
+
+const filters = ['1', '2', '3'];
 
 const Filters = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  console.log('RENDER FILTERS');
+
+  const handleOpenMenu = (e) => {
+    const filterType = e.target.id;
+
+    console.log('OPEN DROP DOwn');
+
+    setIsOpen((prevState) =>
+      prevState !== filterType ? filterType : false
+    );
+  };
+
   return (
-    <>
-      <select>
-        <option></option>
-        <option></option>
-      </select>
-    </>
+    <form className={s.container}>
+      {filters.map((filter) => (
+        <div key={filter} className={s.dropDown}>
+          <button
+            className={s.dropBtn}
+            type='button'
+            id={filter}
+            onClick={handleOpenMenu}
+          ></button>
+          {isOpen === filter && <div className={s.filtersWrap} />}
+        </div>
+      ))}
+    </form>
   );
 };
 
--- src/pages/Pokedex/index.js
+++ src/pages/Pokedex/index.js
@@ -1,6 +1,16 @@
 import React from 'react';
 import PokemonsContainer from '../../components/PokemonsContainer';
 
+<<<<<<< HEAD:src/pages/Pokedex/index.js
+=======
+export const loader = async () => {
+  const pokemonsList = await config.getPokemons();
+  return { pokemonsList };
+};
+
+//TODO: как запрашивать необходиое количество в зависимости от размера экрана
+
+>>>>>>> b02ed70... Add filters buttons and checkbox:src/roots/Pokedex/index.js
 const Pokedex = () => {
   console.log('RENDER POKEDEX');
   return (
