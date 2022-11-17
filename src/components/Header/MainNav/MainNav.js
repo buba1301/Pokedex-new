706e6284d4a3c25a2db42382cde177570e8ee9f6
Boris Muzalevskii <bmuza57@gmail.com>
2022-11-17 17:21:46 +0300

    add render nav menu for different screen width

--- src/components/Header/MainNav/MainNav.js
+++ src/components/Header/MainNav/MainNav.js
@@ -3,45 +3,65 @@ import { Link } from 'react-router-dom';
 import cn from 'classnames';
 
 import s from './MainNav.module';
+import DropDown from '../../../shared/DropDown';
+import useWindowSize from '../../../hooks/useWindowSize';
+import { useButton } from '../../../hooks/useButton';
 
 const menuNames = ['Home', 'Pokedex', 'Legendaries', 'Documentation'];
 
 // TODO: добавить закрытие по клику на ссылку для мобильной версии
 
+const renderNav = () => (
+  <nav aria-label='Main' className={s.nav}>
+    <ul className={s.list}>
+      {menuNames.map((name) => (
+        <li key={name} className={s.navItem}>
+          <Link
+            to={`/${name !== 'Home' ? name.toLowerCase() : ''}`}
+            className={s.link}
+          >
+            {name}
+          </Link>
+        </li>
+      ))}
+    </ul>
+  </nav>
+);
+
 const MainNav = () => {
-  const [isOpen, setIsOpen] = useState(false);
+  // const [isOpen, setIsOpen] = useState(false);
 
-  const handleOpenMenu = () => {
+  const { width } = useWindowSize();
+  const { CloseButton, isOpen } = useButton();
+
+  /* const handleOpenMenu = () => {
     setIsOpen((prevState) => !prevState);
-  };
-
-  const navClass = cn(s.nav, {
-    [s.openNav]: isOpen,
-    [s.closeNav]: !isOpen,
-  });
-
-  const buttonClass = cn(s.button, {
-    [s.active]: isOpen,
-  });
+  };*/
 
   return (
     <>
-      <nav className={navClass} aria-label='Main'>
-        <ul>
-          {menuNames.map((name) => (
-            <li key={name} className={s.navItem}>
-              <Link
-                to={`/${name !== 'Home' ? name.toLowerCase() : ''}`}
-                className={s.link}
-              >
-                {name}
-              </Link>
-            </li>
-          ))}
-        </ul>
-      </nav>
+      {width >= 769 ? (
+        renderNav()
+      ) : (
+        <DropDown isOpen={isOpen} menuType='navMenu'>
+          {renderNav()}
+        </DropDown>
+      )}
 
-      <button
+      <CloseButton isOpen={isOpen} />
+    </>
+  );
+};
+
+export default MainNav;
+
+/*
+
+const buttonClass = cn(s.button, {
+    [s.active]: isOpen,
+  });
+
+<button
         className={buttonClass}
         type='button'
         onClick={handleOpenMenu}
@@ -49,9 +69,4 @@ const MainNav = () => {
         <span className={s.burgerMenu} />
         <span className={s.burgerMenu} />
         <span className={s.burgerMenu} />
-      </button>
-    </>
-  );
-};
-
-export default MainNav;
+      </button> */
