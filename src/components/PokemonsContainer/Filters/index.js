560ff690313812faf8773c30fcd3ffc0f703d35c
Boris Muzalevskii <bmuza57@gmail.com>
2022-11-17 17:19:53 +0300

    Add checkbox elem and DropDown component

--- src/components/PokemonsContainer/Filters/index.js
+++ src/components/PokemonsContainer/Filters/index.js
@@ -1,8 +1,11 @@
-import classNames from 'classnames';
 import React, { useState } from 'react';
+import { useButton } from '../../../hooks/useButton';
 import useWindowSize from '../../../hooks/useWindowSize';
+import Checkbox from '../../../shared/CheckBox';
+import DropDown from '../../../shared/DropDown';
 
 import s from './Filter.module';
+import MobileFilterMenu from './MobileFilterMenu';
 
 const filters = ['types', 'attack', 'experience'];
 
@@ -19,22 +22,27 @@ const filtersValues = {
     'Poison',
     'Ground',
   ],
-  attack: ['50000', '100000', '150000'],
-  experience: ['50000', '100000', '150000'],
+  attack: ['0-30', '30-50', '50-100', '>100'],
+  experience: ['0-30', '30-50', '50-100', '>100'],
 };
 
 //TODO: вынести button в отдельный элемент
+//TODO: выпадающее меню для мобильной версии с фильтрами
+// TODO: запрос отфильтрованных данных
+// TODO: если поиск то сброс фильтров
+// TODO: сохранять фильтры при закрытии меню если выбран чекбокс
+// TODO: кнопка сброса фильтров
+// TODO: удалить крестик в сафари
 
 const Filters = () => {
-  const [isOpen, setIsOpen] = useState(false);
-  console.log('RENDER FILTERS');
+  // const [isOpen, setIsOpen] = useState(false);
 
   const { width } = useWindowSize();
 
+  const { isOpen, onClick, CloseButton } = useButton();
+
   const handleOpenMenu = (e) => {
     const filterType = e.target.id;
-
-    console.log('OPEN DROP DOwn');
 
     setIsOpen((prevState) =>
       prevState !== filterType ? filterType : false
@@ -60,14 +68,7 @@ const Filters = () => {
               <ul className={s.filtersMenu}>
                 {filtersValues[filter].map((value) => (
                   <li className={s.dropDownInput} key={value}>
-                    <label htmlFor={value}>
-                      <input
-                        id={value}
-                        type='checkbox'
-                        className={s.inputCheckbox}
-                      />
-                      {value}
-                    </label>
+                    <Checkbox value={value} />
                   </li>
                 ))}
               </ul>
@@ -75,17 +76,25 @@ const Filters = () => {
           </div>
         ))
       ) : (
-        <div className={s.dropDown}>
-          <button
-            className={s.dropBtn}
-            type='button'
-            id='filters'
-            onClick={handleOpenMenu}
-          >
-            Filters
-            <span>1</span>
-          </button>
-        </div>
+        <>
+          <div className={s.dropDown}>
+            <button
+              className={s.dropBtn}
+              type='button'
+              id='filters'
+              onClick={onClick}
+            >
+              Filters
+              <span>1</span>
+            </button>
+          </div>
+          <DropDown isOpen={isOpen} menuType='filterMenu'>
+            <MobileFilterMenu types={filtersValues.types} />
+            <div className={s.closeBtn}>
+              <CloseButton />
+            </div>
+          </DropDown>
+        </>
       )}
     </form>
   );
